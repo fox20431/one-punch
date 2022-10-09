9e3c557d68d42eeb6244e87287e6f6c95cddf7b0
ming <fox20431@gmail.com>
2022-10-09 16:21:58 +0800

    Finished the CSS come from font feature.

--- src/app/css-come-from-font/css-come-from-font.component.ts
+++ src/app/css-come-from-font/css-come-from-font.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-css-come-from-font',
@@ -8,6 +8,35 @@ import { Component, OnInit } from '@angular/core';
 export class CssComeFromFontComponent implements OnInit {
 
   constructor() { }
+
+	file?: File = undefined
+
+	@ViewChild('fontFile')
+	fontFile!: ElementRef<HTMLInputElement>
+
+	@ViewChild('resultCss')
+	resultCss!: ElementRef<HTMLTextAreaElement>
+
+	onFileSelected = (event: Event) => {
+		this.file = (event.target as HTMLInputElement).files![0]
+	}
+
+	convertToCssFromFontFile = (event: Event) => {
+		let formData = new FormData()
+		formData.append('file', this.file!)
+		fetch('http://warrior.hihusky.com:8080/css-file', {
+			method: 'POST',
+			body: formData
+		})
+		.then( res => {
+			return res.text();
+		})
+		.then ( res => {
+			console.log(res);
+			this.resultCss.nativeElement.innerHTML = res
+		})
+	}
+
 
   ngOnInit(): void {
   }
