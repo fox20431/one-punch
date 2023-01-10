b7dfefd1c17aae97ae389f4e1a80306507ea4c82
fox20431 <fox20431@gmail.com>
2023-01-10 12:31:39 +0800

    proxying to a backend server

--- projects/blog/src/app/content/content.component.ts
+++ projects/blog/src/app/content/content.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
   templateUrl: './content.component.html',
   styleUrls: ['./content.component.scss']
 })
-export class ContentComponent {
+export class ContentComponent implements OnInit {
 
+    constructor(private http: HttpClient) {}
+
+    ngOnInit(): void {
+        let pagedPostsObs = this.http.get("/posts?page=0&size=10")
+        pagedPostsObs.subscribe((data) => {console.log(data)})
+        throw new Error('Method not implemented.');
+    }
+    
 }
--- projects/dashboard/src/app/app.module.ts
+++ projects/dashboard/src/app/app.module.ts
@@ -1,22 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { HttpClientModule } from '@angular/common/http'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
-import { ContentComponent } from './content/content.component';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    ProfileComponent,
-    ContentComponent
+    AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule,
-    HttpClientModule
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
